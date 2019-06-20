12907607462fa5523acf522d6428c7f572bd361c
Jyoti <jyotime6678@gmail.com>
2019-06-20 16:23:43 +0530

    Read the text and Pass into API

--- editor/serializers.py
+++ editor/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
+from django.conf import settings
+import docx2txt
 from .models import *
 
 
 
 class FileSerializer(serializers.ModelSerializer):
 	
+	detail = serializers.SerializerMethodField('details')
 	class Meta:
 		model = fileUpload
 		fields= '__all__'
+
+	def details(self,obj):
+		return docx2txt.process(settings.BASE_DIR+obj.file.url)
